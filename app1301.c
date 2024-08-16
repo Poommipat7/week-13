494bd5a42799c0bbd0eebc29fafe04296293a916
ReMakeFew <goodgamezza007@gmail.com>
2024-08-16 15:15:55 +0700

    first commit

---
+++ app1301.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+void main()
+{
+    //char name[] = "Mr.Markb Zuckerberg";
+    char name[5] = {'M', 'a', 'r', 'k', '\0'};
+    printf("My Name is %s", name);
+}
---
+++ app1302.c
@@ -0,0 +1,9 @@
+int main(void)
+{
+    char str[20];
+    printf("Enter text:");
+    // gets(str);
+    scanf("%s", &str);
+    printf("Output: %s\n", str);
+    return (0);
+}
